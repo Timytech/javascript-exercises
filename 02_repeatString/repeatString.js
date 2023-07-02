a21224e1b4321140de3f8009fb2a6000bc0b0042
Timytech <timpiano699@gmail.com>
2023-07-02 23:32:05 +0000

    Complete javascript-exercise 01-helloWorld

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let str2 = '';
+    for(let i = 1; i <= num; i++){
+        str2 += str;
+    }
+    if(num < 0){
+        return 'ERROR'
+    }
+    return str2;
 };
+repeatString('BBB', -1)
 
 // Do not edit below this line
 module.exports = repeatString;

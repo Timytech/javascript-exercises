9641005c9a7557bda5eb70371a0a8d5c23b669b4
Timytech <timpiano699@gmail.com>
2023-07-04 11:38:20 +0000

    Complete javascript-exercise-06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //check if year is divisible by 100 first, then check 400
+    //if year is not divisible by 100, check if year is divisible by 4
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
+console.log(leapYears(2020));
+

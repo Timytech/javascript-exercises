7d1305612c01197041994dc95e9005c993cb8c25
Timytech <timpiano699@gmail.com>
2023-07-21 22:42:49 +0000

    Complete javascript-exercise-08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, value) => total + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let result = 1;
+  for(let i=0; i<args.length; i++){
+    result *= args[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0) return 1;
+  let result = 1;
+  for(let i=x; i>0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

e45e88b886964e5e6482345fde5c134a6db694f8
Timytech <timpiano699@gmail.com>
2023-07-23 14:51:04 +0000

    Complete javascript-exercise-10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let x = parseInt(number);
+    if(x < 0) return "OOPS";
 
+    const fibSequence = [0, 1];
+    for(let i = 1; i < x; i++){
+        fibSequence.push(fibSequence[i] + fibSequence[i-1]);
+    }
+    return fibSequence[x];
 };
 
 // Do not edit below this line

5b84f96bd53edd32928a255011ca4318bdec478c
Timytech <timpiano699@gmail.com>
2023-08-07 11:01:46 +0000

    Complete javascript-exercise-11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map((book) => book.title);
 };
+
+//========================================================================
+//Simple array check
+const books = [
+    {
+      title: 'Boomerang',
+      author: 'Nicolas'
+    },
+    {
+      title: 'Cultural Heritage',
+      author: 'Adolphus'
+    }
+  ]
+
+console.log(getTheTitles(books));
+//========================================================================
 
 // Do not edit below this line
 module.exports = getTheTitles;
+

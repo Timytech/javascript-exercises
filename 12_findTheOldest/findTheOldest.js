86d0b272e5b9614396535ae107605c2c8d1b18c1
Timytech <timpiano699@gmail.com>
2023-08-07 11:44:14 +0000

    Complete javascript exercise-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (array) {
+    return array.reduce((person1, person2) => {
+      const person1Age = getAge(person1.yearOfBirth, person1.yearOfDeath);
+      const person2Age = getAge(
+        person2.yearOfBirth,
+        person2.yearOfDeath
+      );
+      return person1Age > person2Age ? person1 : person2;
+    });
+  };
+  
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
 // Do not edit below this line
 module.exports = findTheOldest;
